84412259dd10ff782218ab7b2f73f2ca86fc1bb4
thanhhai1998 <buithanhhai1998@gmail.com>
2019-04-01 21:56:12 +0700

    bai thuc hanh lap 4 5 6

---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BigSchool.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
