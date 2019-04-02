45855290e80404299cb2c0584d2ee2c86585d688
thanhhai1998 <buithanhhai1998@gmail.com>
2019-04-02 14:33:09 +0700

    xong

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -9,6 +9,7 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        public int Id { get; set; }
         [Required]
         public string Place { get; set; }
 
@@ -26,6 +27,11 @@ namespace BigSchool.ViewModels
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+        public string Heading { get; set; }
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
         }
        
     }
